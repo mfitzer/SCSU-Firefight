b49791a2829d61ab39a3449c872d4125849a6a09
Marty Fitzer <m4fitzer@gmail.com>
2019-04-07 15:08:00 -0500

    Firefighter Silhouette Implemented
    - ISELF building is made invisible on start
    - Also got a walking animation
    - Firefighter model needs to be replaced

---
+++ Assets/InvisibilityController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class InvisibilityController : MonoBehaviour
+{
+    public Material invisibleMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enableInvisibility(transform);
+    }
+
+    public void enableInvisibility(Transform parent)
+    {
+        Renderer rend = parent.GetComponent<Renderer>();
+
+        if (rend)
+        {
+            rend.material = invisibleMaterial;
+        }
+
+        //Change materials in children
+        foreach (Transform child in parent.transform)
+        {
+            if (child != parent)
+            {
+                enableInvisibility(child);
+            }
+        }
+    }
+}
